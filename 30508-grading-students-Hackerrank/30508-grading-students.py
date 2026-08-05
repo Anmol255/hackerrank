7561922c957dbb5e817b7adbc8ba6c557e3ba0f5
Anmol255 <anmolk2201@gmail.com>
2026-08-05 22:48:50 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 30508-grading-students-Hackerrank/30508-grading-students.py
@@ -0,0 +1,50 @@
+#!/bin/user/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    rounded_grades = []
+    for grade in grades:
+        if grade >= 38:
+            next_multiple_of_5 = ((grade // 5) + 1) * 5
+            if next_multiple_of_5 - grade < 3:
+                rounded_grades.append(next_multiple_of_5)
+            else:
+                rounded_grades.append(grade)
+        else:
+            rounded_grades.append(grade)
+    return rounded_grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
