9fd6f7b9971913099b939ae4cd5f35c06bee2d25
Anmol255 <anmolk2201@gmail.com>
2026-09-04 21:46:44 +0530

    Force committed solution - Time: O(1), Space: O(1)

--- 25168-sales-by-match-Hackerrank/25168-sales-by-match.py
+++ 25168-sales-by-match-Hackerrank/25168-sales-by-match.py
@@ -16,34 +16,10 @@ import sys
 #
 
 def sockMerchant(n, ar):
-    # Use a set to keep track of socks looking for a match
-    unmatched_socks = set()
-    pairs = 0
-    
-    for sock in ar:
-        # If we already have one matching sock in the set, we found a pair!
-        if sock in unmatched_socks:
-            pairs += 1
-            unmatched_socks.remove(sock) # Remove it since the pair is complete
-        else:
-            # Otherwise, add this sock to wait for its match
-            unmatched_socks.add(sock)
-            
-    return pairs
+    # Write your code here
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = sockMerchant(n, ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
