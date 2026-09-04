2d7769da257ea8607a41dfceb49b5b280ad66e4e
Anmol255 <anmolk2201@gmail.com>
2026-09-04 21:47:30 +0530

    Force committed solution - Time: O(1), Space: O(1)

--- 22564-drawing-book-Hackerrank/22564-drawing-book.py
+++ 22564-drawing-book-Hackerrank/22564-drawing-book.py
@@ -17,28 +17,9 @@ import sys
 
 def pageCount(n, p):
     # Write your code here
-    # Turns from the front
-    front_turns = p // 2
-    
-    # Turns from the back
-    back_turns = (n // 2) - (p // 2)
-    
-    # Return the minimum of both directions
-    return min(front_turns, back_turns)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    p = int(input().strip())
-
-    result = pageCount(n, p)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
-
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
